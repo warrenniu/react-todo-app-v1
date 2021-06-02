cf4b41af81c8870ddb69a3c9693080532706cbf2
warrenniu <warrenniu@gmail.com>
2021-06-02 14:20:20 -0400

    connected to do form with to do list. Working on button functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
+// import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>To do List App</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
     const [input, setInput] = useState('');
 
     const handleSubmit = event => {
         event.preventDefault()
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 100000),
+            text: input
+        });
+
+        setInput("")
     };
+
+    const handleChange = event => {
+        setInput(event.target.value)
+    }
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
@@ -15,6 +26,7 @@ function TodoForm() {
                 value={input} 
                 name="text" 
                 className="todo-input"
+                onChange={handleChange}
             >
             </input>
             <button className="todo-button">Add todo</button>
